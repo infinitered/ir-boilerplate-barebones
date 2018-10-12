b0d9b4028e94407e2cca29cba07c18ecd2d36541
Jamon Holmgren <jamon@infinite.red>
2018-10-12 16:35:33 -0700

    Initial

---
+++ boilerplate.js
@@ -0,0 +1,78 @@
+const { merge, pipe, assoc, omit, __ } = require('ramda')
+const { getReactNativeVersion, isAndroidInstalled } = require('./lib/react-native-utils')
+
+/**
+ * Let's install.
+ *
+ * @param {any} context - The gluegun context.
+ */
+async function install(context) {
+  const {
+    filesystem,
+    parameters,
+    ignite,
+    reactNative,
+    print,
+    system,
+    template
+  } = context
+  const { colors } = print
+  const { red, yellow, bold, gray, blue } = colors
+
+  const perfStart = (new Date()).getTime()
+
+  const name = parameters.third
+  const spinner = print
+    .spin(`using the ${red('Infinite Red')} barebones React Native boilerplate`)
+
+  // attempt to install React Native or die trying
+  const rnInstall = await reactNative.install({
+    name,
+    version: getReactNativeVersion(context)
+  })
+  if (rnInstall.exitCode > 0) process.exit(rnInstall.exitCode)
+
+  // patch third-party issue with React Native 0.57.3
+  system.run('./node_modules/react-native/scripts/ios-install-third-party.sh')
+
+  spinner.succeed(`patched Xcode 10 issue`)
+
+  // git configuration
+  const gitExists = await filesystem.exists('./.git')
+  if (!gitExists && !parameters.options['skip-git'] && system.which('git')) {
+    // initial git
+    const spinner = print.spin('configuring git')
+
+    // TODO: Make husky hooks optional
+    const huskyCmd = '' // `&& node node_modules/husky/bin/install .`
+    system.run(`git init . && git add . && git commit -m "Initial commit." ${huskyCmd}`)
+
+    spinner.succeed(`configured git`)
+  }
+
+  const perfDuration = parseInt(((new Date()).getTime() - perfStart) / 10) / 100
+  spinner.succeed(`ignited ${yellow(name)} in ${perfDuration}s`)
+
+  const androidInfo = isAndroidInstalled(context) ? ''
+    : `\n\nTo run in Android, make sure you've followed the latest react-native setup instructions at https://facebook.github.io/react-native/docs/getting-started.html before using ignite.\nYou won't be able to run ${bold('react-native run-android')} successfully until you have.`
+
+  const successMessage = `
+    ${red('Ignite CLI')} ignited ${yellow(name)} in ${gray(`${perfDuration}s`)}
+
+    To get started:
+
+      cd ${name}
+      react-native run-ios (you may need to run this twice)
+      react-native run-android${androidInfo}
+
+    ${blue('Need additional help? Join our Slack community at http://community.infinite.red.')}
+
+    ${bold('Now get cooking! 🍽')}
+  `
+
+  print.info(successMessage)
+}
+
+module.exports = {
+  install
+}
